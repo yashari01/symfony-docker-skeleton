e3b3b8e38e3b56147bdd197d7c26ae880969208c
M'SOUBER AHMED <amsouber@ippon.ma>
2020-11-02 17:20:08 +0100

    test bootstrap

---
+++ src/tests/Entity/UserTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testCreate()
+    {
+       /* $this->createMock(DinosaurLengthDeterminator::class);*/
+        $this->assertTrue(true);
+    }
+}
