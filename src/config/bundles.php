a5e7fb8e657b91919a7ef1a2603908221111b89d
M'SOUBER AHMED <amsouber@ippon.ma>
2020-11-05 19:58:25 +0100

    liip_imagine

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -12,4 +12,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
